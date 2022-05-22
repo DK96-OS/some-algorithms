3767483c5979f8d9a2dfa8f0576aa6a947079644
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-05-22 12:47:52 -0400

    Algorithms: * Create IndexTraversalEnhancedAlgorithm

---
+++ main/src/main/java/io/github/dk96os/algorithms/IndexTraversalEnhancedAlgorithm.java
@@ -0,0 +1,28 @@
+package io.github.dk96os.algorithms;
+
+/** Searches secondary indices in reverse order.
+ * @author DK96-OS : 2022
+ */
+public class IndexTraversalEnhancedAlgorithm
+	implements MaxDistanceInterface {
+
+	@Override
+	public int searchMaxDistance(
+		final int[] array
+	) {
+		int distance = 0;
+		for (
+			int i = 0; i < array.length; ++i
+		) for (
+			int j = array.length - 1; j > i; --j
+		) {
+			if (array[i] == array[j]) {
+				distance = Math.max(
+					distance, j - i
+				);
+			}
+		}
+		return distance;
+	}
+
+}
