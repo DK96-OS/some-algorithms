e7fbfa8b5f84a02f19463a4027ed39bfb95a1cf5
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2023-09-02 23:25:03 -0400

    Test: * Update AlgorithmPseudoRandomBench2 - add dataset 3, add dataset 4

--- maxdistance/src/test/java/maxdistance/bench/AlgorithmPseudoRandomBench2.java
+++ maxdistance/src/test/java/maxdistance/bench/AlgorithmPseudoRandomBench2.java
@@ -24,6 +24,16 @@ public final class AlgorithmPseudoRandomBench2 {
 	private static PseudorandomDistribution data2;
 	private static final int data2Answer = 48717;
 
+	/** Dataset 3
+	 */
+	private static PseudorandomDistribution data3;
+	private static final int data3Answer = 46920;
+
+	/** Dataset 4 : MaxDistance = Zero
+	 */
+	private static PseudorandomDistribution data4;
+	private static final int data4Answer = 0;
+
 	@BeforeAll
 	public static void testSetup() {
 		data1 = new PseudorandomDistribution(
@@ -31,6 +41,12 @@ public final class AlgorithmPseudoRandomBench2 {
 		);
 		data2 = new PseudorandomDistribution(
 			50_000, -1_000_000, 1_000_000, 12345L
+		);
+		data3 = new PseudorandomDistribution(
+			50_000, -10_000_000, 10_000_000, 12345L
+		);
+		data4 = new PseudorandomDistribution(
+			50_000, -1_000_000_000, 1_000_000_000, 12345L
 		);
 	}
 
@@ -59,6 +75,38 @@ public final class AlgorithmPseudoRandomBench2 {
 	) {
 		long timeRequired = AlgorithmBenchmarkHelper.nanoTime(
 			algorithm, data2.getArray(), data2Answer
+		);
+		AlgorithmBenchmarkHelper.print(
+			algorithm, timeRequired
+		);
+	}
+
+	@Tag("Slow")
+	@ParameterizedTest
+	@ArgumentsSource(
+		AlgorithmArgumentsProvider.class
+	)
+	public void testDataset3(
+		final MaxDistanceAlgorithm algorithm
+	) {
+		long timeRequired = AlgorithmBenchmarkHelper.nanoTime(
+			algorithm, data3.getArray(), data3Answer
+		);
+		AlgorithmBenchmarkHelper.print(
+			algorithm, timeRequired
+		);
+	}
+
+	@Tag("Slow")
+	@ParameterizedTest
+	@ArgumentsSource(
+		AlgorithmArgumentsProvider.class
+	)
+	public void testDataset4(
+		final MaxDistanceAlgorithm algorithm
+	) {
+		long timeRequired = AlgorithmBenchmarkHelper.nanoTime(
+			algorithm, data4.getArray(), data4Answer
 		);
 		AlgorithmBenchmarkHelper.print(
 			algorithm, timeRequired
